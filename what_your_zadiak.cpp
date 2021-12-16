16fedebdcf2f241a9ca3fab26c200adcbd91a6ef
MIgor <j.bond-007@mail.ru>
2021-12-16 18:33:22 +1000

    Введен вечный цикл. В функции display_zodiak введен оператор return.

--- what_your_zadiak.cpp
+++ what_your_zadiak.cpp
@@ -10,18 +10,66 @@ using namespace std;
 
 void display_zodiak(int zodiac)
 {
-	if (zodiac >= 121 && zodiac <= 219)   cout << "водолей\n";
-	if (zodiac >= 220 && zodiac <= 320)   cout << "рыбы\n";
-	if (zodiac >= 321 && zodiac <= 420)   cout << "овен\n";
-	if (zodiac >= 421 && zodiac <= 521)   cout << "телец\n";
-	if (zodiac >= 522 && zodiac <= 621)   cout << "близнецы\n";
-	if (zodiac >= 621 && zodiac <= 722)   cout << "рак\n";
-	if (zodiac >= 723 && zodiac <= 823)   cout << "лев\n";
-	if (zodiac >= 824 && zodiac <= 923)   cout << "дева\n";
-	if (zodiac >= 924 && zodiac <= 1023)  cout << "весы\n";
-	if (zodiac >= 1024 && zodiac <= 1122) cout << "скорпион\n";
-	if (zodiac >= 1123 && zodiac <= 1221) cout << "стрелец\n";
-	if (zodiac >= 1222 || zodiac <= 120)  cout << "козерог\n";
+	if (zodiac >= 121 && zodiac <= 219)
+	{
+		cout << "\nВаш знак зодиака - водолей\n";
+		return;
+	}
+	if (zodiac >= 220 && zodiac <= 320)
+	{
+		cout << "\nВаш знак зодиака - рыбы\n";
+		return;
+	}
+	if (zodiac >= 321 && zodiac <= 420)
+	{
+		cout << "\nВаш знак зодиака - овен\n";
+		return;
+	}
+	if (zodiac >= 421 && zodiac <= 521)
+	{
+		cout << "\nВаш знак зодиака - телец\n";
+		return;
+	}
+	if (zodiac >= 522 && zodiac <= 621)
+	{
+		cout << "\nВаш знак зодиака - близнецы\n";
+		return;
+	}
+	if (zodiac >= 621 && zodiac <= 722)
+	{
+		cout << "\nВаш знак зодиака - рак\n";
+		return;
+	}
+	if (zodiac >= 723 && zodiac <= 823)
+	{
+		cout << "\nВаш знак зодиака - лев\n";
+		return;
+	}
+	if (zodiac >= 824 && zodiac <= 923)
+	{
+		cout << "\nВаш знак зодиака - дева\n";
+		return;
+	}
+	if (zodiac >= 924 && zodiac <= 1023)
+	{
+		cout << "\nВаш знак зодиака - весы\n";
+		return;
+	}
+	if (zodiac >= 1024 && zodiac <= 1122)
+	{
+		cout << "\nВаш знак зодиака - скорпион\n";
+		return;
+	}
+	if (zodiac >= 1123 && zodiac <= 1221)
+	{
+		cout << "\nВаш знак зодиака - стрелец\n";
+		return;
+	}
+	if (zodiac >= 1222 || zodiac <= 120)
+	{
+		cout << "\nВаш знак зодиака - козерог\n";
+		return;
+	}
 }
 
 int main()
@@ -31,16 +79,32 @@ int main()
 
 	int num, day, month;
 
-	cout << "Enter day of your Birthday\n";
-	cin >> day;
-	cout << "Enter month of your Birthday\n";
-	cin >> month;
-	num = 100 * month + day;
+	while (true)
+	{
+		system("cls");
 
-	display_zodiak(num);
+		cout << "Enter day of your Birthday\n";
+		cin >> day;
+		cout << "Enter month of your Birthday\n";
+		cin >> month;
+
+		if (day && month)
+		{
+			num = 100 * month + day;
+
+			display_zodiak(num);
+
+			cout << endl << endl;
+			system("pause");
+		}
+		else
+		{
+			break;
+		}
+	}
 
 
-	cout << endl << endl;
+	cout << endl << endl << "Программа завершена!" << endl << endl;
 	system("pause");
 	return EXIT_SUCCESS;
 }
